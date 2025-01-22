969498706b70ddf2798ab5f7c5954f56b6bd41ed
a <>
2025-01-22 21:06:14 +0900

    no message

--- app/src/main/java/com/example/iceinwonderland/data/EndlessScrollView.java
+++ app/src/main/java/com/example/iceinwonderland/data/EndlessScrollView.java
@@ -32,7 +32,7 @@ public class EndlessScrollView extends View {
     }
 
     private void init(){
-        //
+        //初期位置？
         backgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ground);
         paint = new Paint();
 
@@ -40,28 +40,36 @@ public class EndlessScrollView extends View {
     }
 
     private void startBackgroundAnimation(){
+        //どれくらい動かしたいか？
         ValueAnimator animator = ValueAnimator.ofFloat(getHeight()+((float)backgroundBitmap.getHeight()/5),0);
+        //何ミリ秒？
         animator.setDuration(1000);
+        //何回？
         animator.setRepeatCount(ValueAnimator.INFINITE);
+        //経過時間の最後に達したらどうするか
         animator.setRepeatMode(ValueAnimator.RESTART);
+        //経過時間の割合を計算するために使用する時間補間器
         animator.setInterpolator(new LinearInterpolator());
-
+        //リスナーを渡す
         animator.addUpdateListener(animation -> {
+            //変化したbackgroundYのfloat受け取る
            backgroundY = (float) animation.getAnimatedValue();
            invalidate();
         });
-
+        //アニメーションスタート！
         animator.start();
     }
 
     @Override
+    //onDraw=Viewを描画する時に呼び出される
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
-
+        //diff=データの違いを示すための単語
         int diff = 0;
         if(getWidth()<= backgroundBitmap.getWidth()) {
             diff = (getWidth() - backgroundBitmap.getWidth()) / 2;
         }
+        //Canvasクラスのメソッド指定されたBitmapを描画する
         canvas.drawBitmap(backgroundBitmap, diff,-getHeight() + backgroundY, paint);
         if(backgroundY >= getHeight()){
             backgroundY = 0;
